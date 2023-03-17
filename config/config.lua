b4c567d743cb986a6f66befd47463cb1775568c5
Dietrich <102512250+Dietrich-io@users.noreply.github.com>
2023-03-18 00:16:01 +0100

    changed models

--- config/config.lua
+++ config/config.lua
@@ -24,7 +24,7 @@ Config.trashcans ={
         coords = vector3(-269.36944580078125, 783.7395629882812, 117.49858093261719),
         heading = 0.0,
         radius= 1.5,
-        model = "p_barrel_ladle01x",
+        model = "p_streettrashcannbx01x",
         itemLimit = 100,          --item limit for all items
         limitedItems = {            --limit specific items
 
@@ -36,7 +36,7 @@ Config.trashcans ={
         coords = vector3(-805.9784545898438, -1330.6209716796875, 42.67081451416015),
         heading = 0.0,
         radius= 1.5,
-        model = "p_barrel_ladle01x",
+        model = "p_streettrashcannbx01x",
         itemLimit = 100,          --item limit for all items
         limitedItems = {            --limit specific items
 
@@ -48,7 +48,7 @@ Config.trashcans ={
         coords = vector3(-742.5406494140625, -1246.8414306640625, 42.45254898071289),
         heading = 0.0,
         radius= 1.5,
-        model = "p_barrel_ladle01x",
+        model = "p_streettrashcannbx01x",
         itemLimit = 100,          --item limit for all items
         limitedItems = {            --limit specific items
 
@@ -60,7 +60,7 @@ Config.trashcans ={
         coords = vector3(1236.4532470703125, -1282.3831787109375, 74.91667175292969),
         heading = 0.0,
         radius= 1.5,
-        model = "p_barrel_ladle01x",
+        model = "p_streettrashcannbx01x",
         itemLimit = 100,          --item limit for all items
         limitedItems = {            --limit specific items
 
@@ -72,7 +72,7 @@ Config.trashcans ={
         coords = vector3(1330.7415771484375, -1316.051025390625, 75.93418884277344),
         heading = 0.0,
         radius= 1.5,
-        model = "p_barrel_ladle01x",
+        model = "p_streettrashcannbx01x",
         itemLimit = 100,          --item limit for all items
         limitedItems = {            --limit specific items
 
@@ -84,7 +84,7 @@ Config.trashcans ={
         coords = vector3(2939.04736328125, 1327.7305908203125, 43.07277679443359),
         heading = 0.0,
         radius= 1.5,
-        model = "p_barrel_ladle01x",
+        model = "p_streettrashcannbx01x",
         itemLimit = 100,          --item limit for all items
         limitedItems = {            --limit specific items
 
@@ -96,7 +96,7 @@ Config.trashcans ={
         coords = vector3(2936.826904296875, 1274.71142578125, 43.63825225830078),
         heading = 0.0,
         radius= 1.5,
-        model = "p_barrel_ladle01x",
+        model = "p_streettrashcannbx01x",
         itemLimit = 100,          --item limit for all items
         limitedItems = {            --limit specific items
 
